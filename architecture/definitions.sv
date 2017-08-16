eada00b9206191dfa763e144ecb93d0f497b3b9d
TheOrangeLego <gzjosef@gmail.com>
2017-08-16 04:53:08 -0700

    Dropped support for overflow function code.

--- architecture/definitions.sv
+++ architecture/definitions.sv
@@ -15,23 +15,24 @@ package definitions;
   const logic [FLAG_WIDTH - 1:0] FLAG_OPERATION   = 3'b001;
 
   /* operation function codes */
-  const logic [FUNC_WIDTH - 1:0] FUNC_ADD = 5'b00000;
-  const logic [FUNC_WIDTH - 1:0] FUNC_SUB = 5'b00001;
-  const logic [FUNC_WIDTH - 1:0] FUNC_ULS = 5'b00010;
-  const logic [FUNC_WIDTH - 1:0] FUNC_URS = 5'b00011;
-  const logic [FUNC_WIDTH - 1:0] FUNC_SLS = 5'b00100;
-  const logic [FUNC_WIDTH - 1:0] FUNC_SRS = 5'b00101;
-  const logic [FUNC_WIDTH - 1:0] FUNC_AND = 5'b00110;
-  const logic [FUNC_WIDTH - 1:0] FUNC_OR  = 5'b00111;
-  const logic [FUNC_WIDTH - 1:0] FUNC_XOR = 5'b01000;
-  const logic [FUNC_WIDTH - 1:0] FUNC_NOT = 5'b01001;
-  const logic [FUNC_WIDTH - 1:0] FUNC_LSS = 5'b01010;
-  const logic [FUNC_WIDTH - 1:0] FUNC_EQL = 5'b01011;
-  const logic [FUNC_WIDTH - 1:0] FUNC_GTR = 5'b01100;
-  const logic [FUNC_WIDTH - 1:0] FUNC_OVF = 5'b01101;
+  const logic [FUNC_WIDTH - 1:0] FUNC_SET = 5'b00000;
+  const logic [FUNC_WIDTH - 1:0] FUNC_ADD = 5'b00001;
+  const logic [FUNC_WIDTH - 1:0] FUNC_SUB = 5'b00010;
+  const logic [FUNC_WIDTH - 1:0] FUNC_ULS = 5'b00011;
+  const logic [FUNC_WIDTH - 1:0] FUNC_URS = 5'b00100;
+  const logic [FUNC_WIDTH - 1:0] FUNC_SLS = 5'b00101;
+  const logic [FUNC_WIDTH - 1:0] FUNC_SRS = 5'b00110;
+  const logic [FUNC_WIDTH - 1:0] FUNC_AND = 5'b00111;
+  const logic [FUNC_WIDTH - 1:0] FUNC_OR  = 5'b01000;
+  const logic [FUNC_WIDTH - 1:0] FUNC_XOR = 5'b01001;
+  const logic [FUNC_WIDTH - 1:0] FUNC_NOT = 5'b01010;
+  const logic [FUNC_WIDTH - 1:0] FUNC_LSS = 5'b01011;
+  const logic [FUNC_WIDTH - 1:0] FUNC_EQL = 5'b01100;
+  const logic [FUNC_WIDTH - 1:0] FUNC_GRT = 5'b01101;
   const logic [FUNC_WIDTH - 1:0] FUNC_ABS = 5'b01110;
 
   /* reserved instructions */
   const logic [INSN_WIDTH - 1:0] INSN_NOP = 16'h1;
   const logic [INSN_WIDTH - 1:0] INSN_HLT = 16'h0;
 endpackage
+
